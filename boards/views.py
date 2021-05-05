a6bfaef19ad19e99ec83e1affdb60f0f74c32010
satomomo0222 <satomomo0222@gmail.com>
2021-05-05 16:35:20 +0900

    掲示板作成画面を作成

---
+++ boards/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib import messages
+
+def create_theme(request):
+    create_theme_form = forms.CreateThemeForm(request.POST or None)
+    if create_theme_form.is_valid():
+        create_theme_form.instance.user = request.user
+        create_theme_form.save()
+        messages.success(request, '掲示板を作成しました')
+        return redirect('accounts:home')
+    return render(
+        request, 'boards/create_theme.html', context={
+            'create_theme_form':create_theme_form,
+        }
+    )
